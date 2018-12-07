dd1fa43d024f4a3ef88466503e4f7f3ca54a3555
caizhenglong <caizl2002@hotmail.com>
2018-12-07 15:57:14 +0800

    remove test for fake.js as it's no use now

--- test/index.js
+++ test/index.js
@@ -1,13 +1,10 @@
 const argv = require('minimist')(process.argv.slice(2))
 
-if (argv.f) {
-  require('./fake.js')
-} else if (argv.a) {
+if (argv.a) {
   require('./api.js')
 } else if (argv.t) {
   require('./transactionRunner.js')
 } else {
-  require('./fake.js')
   require('./api.js')
   require('./transactionRunner.js')
 }
