daf2c4929507fff53b370152613eff0c9c996c41
caizhenglong <caizl2002@hotmail.com>
2018-12-11 10:30:03 +0800

    combine code from utils.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const Bytes = require('eth-lib/lib/bytes')
 const utils = require('./utils.js')
 const Hash = require('eth-lib/lib/hash')
 const secp256k1 = require('secp256k1')
+const numberToBN = require('number-to-bn')
+const _ = require('underscore')
+const Buffer = require('safe-buffer').Buffer
 
 // secp256k1n/2
 const N_DIV_2 = new BN('7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0', 16)
@@ -322,8 +325,8 @@ class Transaction {
       transaction.to = tx.to || '0x' // Can be zero, for contract creation
       transaction.data = tx.data || '0x' // can be zero for general TXs
       transaction.value = tx.value || '0x' // can be zero for contract call
-      transaction.chainId = utils.numberToHex(tx.chainId)
-      transaction.shardingFlag = utils.numberToHex(tx.shardingFlag)
+      transaction.chainId = numberToHex(tx.chainId)
+      transaction.shardingFlag = numberToHex(tx.shardingFlag)
       transaction.systemContract = '0x0' // System contract flag, always = 0
       transaction.via = tx.via || '0x' // vnode subchain address
 
@@ -355,9 +358,9 @@ class Transaction {
 
       // Add trimLeadingZero to avoid '0x00' after makeEven
       // don't allow uneven r,s,v values
-      rawTx[vPos] = utils.trimLeadingZero(utils.makeEven(utils.bufferToHex(newV)))
-      rawTx[vPos + 1] = utils.trimLeadingZero(utils.makeEven(utils.bufferToHex(newsign.r)))
-      rawTx[vPos + 2] = utils.trimLeadingZero(utils.makeEven(utils.bufferToHex(newsign.s)))
+      rawTx[vPos] = trimLeadingZero(makeEven(bufferToHex(newV)))
+      rawTx[vPos + 1] = trimLeadingZero(makeEven(bufferToHex(newsign.r)))
+      rawTx[vPos + 2] = trimLeadingZero(makeEven(bufferToHex(newsign.s)))
       return RLP.encode(rawTx)
     } catch (e) {
       return e
@@ -420,6 +423,100 @@ class Transaction {
       return errors.join(' ')
     }
   }
+}
+/**
+ * This function is to resolve the issue
+ * https://github.com/ethereum/web3.js/issues/1170
+ */
+function makeEven (hex) {
+  if (hex.length % 2 === 1) {
+    hex = hex.replace('0x', '0x0')
+  }
+  return hex
+}
+
+/**
+ * Takes an input and transforms it into an BN
+ *
+ * @method toBN
+ * @param {Number|String|BN} number, string, HEX string or BN
+ * @return {BN} BN
+ */
+function toBN (number) {
+  try {
+    return numberToBN.apply(null, arguments)
+  } catch (e) {
+    throw new Error(e + ' Given value: "' + number + '"')
+  }
+}
+
+/**
+ * Converts value to it's hex representation
+ *
+ * @method numberToHex
+ * @param {String|Number|BN} value
+ * @return {String}
+ */
+function numberToHex (value) {
+  if (_.isNull(value) || _.isUndefined(value)) {
+    return value
+  }
+
+  if (!isFinite(value) && !moacUtil.isHexString(value)) {
+    throw new Error('Given input "' + value + '" is not a number.')
+  }
+
+  var number = toBN(value)
+  var result = number.toString(16)
+
+  return number.lt(new BN(0)) ? '-0x' + result.substr(1) : '0x' + result
+}
+
+// To fix an error of 2 leading 0s
+function trimLeadingZero (hex) {
+  while (hex && hex.startsWith('0x00')) {
+    hex = '0x' + hex.slice(4)
+  }
+  return hex
+}
+
+/**
+ * Attempts to turn a value into a `Buffer`. As input it supports `Buffer`, `String`, `Number`,
+ * null/undefined, `BN` and other objects with a `toArray()` method.
+ * @param {*} v the value
+ */
+function toBuffer (v) {
+  if (!Buffer.isBuffer(v)) {
+    if (Array.isArray(v)) {
+      v = Buffer.from(v)
+    } else if (typeof v === 'string') {
+      if (moacUtil.isHexString(v)) {
+        v = Buffer.from(moacUtil.padToEven(moacUtil.stripHexPrefix(v)), 'hex')
+      } else {
+        v = Buffer.from(v)
+      }
+    } else if (typeof v === 'number') {
+      v = moacUtil.intToBuffer(v)
+    } else if (v === null || v === undefined) {
+      v = Buffer.allocUnsafe(0)
+    } else if (v.toArray) {
+      // converts a BN to a Buffer
+      v = Buffer.from(v.toArray())
+    } else {
+      throw new Error('invalid type')
+    }
+  }
+  return v
+}
+
+/**
+ * Converts a `Buffer` into a hex `String`
+ * @param {Buffer} buf
+ * @return {String}
+ */
+function bufferToHex (buf) {
+  buf = toBuffer(buf)
+  return '0x' + buf.toString('hex')
 }
 
 module.exports = Transaction
