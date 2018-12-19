4ad829942f5995961a5eb496bd963dfc6a822aaf
caizhenglong <caizl2002@hotmail.com>
2018-12-19 10:15:23 +0800

    update comment

--- index.js
+++ index.js
@@ -44,15 +44,15 @@ function ecsign (msgHash, privateKey) {
  *
  * @example
  * var rawTx = {
- *   nonce: '0x00',
- *   gasPrice: '0x09184e72a000',
- *   gasLimit: '0x2710',
- *   to: '0x0000000000000000000000000000000000000000',
- *   value: '0x00',
- *   data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
- *   v: '0x1c',
- *   r: '0x5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab',
- *   s: '0x5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13'
+  nonce: '0x00',
+  gasPrice: '0x09184e72a000',
+  gasLimit: '0x2710',
+  to: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
+  shardingFlag: 0,
+  // chainId - mainnet: 99, testnet: 101
+  chainId: 101
  * };
  * var tx = new Transaction(rawTx);
  *
@@ -70,9 +70,7 @@ function ecsign (msgHash, privateKey) {
  * @param {Buffer} data.to to the to address
  * @param {Buffer} data.value the amount of moac sent
  * @param {Buffer} data.data this will contain the data of the message or the init of a contract
- * @param {Buffer} data.v EC recovery ID
- * @param {Buffer} data.r EC signature parameter
- * @param {Buffer} data.s EC signature parameter
+ * @param {Buffer} data.shardingFlag choose sharding or not
  * @param {Number} data.chainId moac chainId - mainnet: 99, testnet: 101
  * */
 class Transaction {
